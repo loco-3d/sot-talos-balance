a9a09bea608c928116d6b0d17eded7977e442a9e
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-11-05 11:28:21 +0100

    nmpc cython

---
+++ src/libs/sum.pxd
@@ -0,0 +1,3 @@
+cdef class Sum:
+    cdef public double a, b
+    cpdef double compute_sum(self)
---
+++ src/libs/sum.py
@@ -0,0 +1,8 @@
+class Sum:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+
+    def compute_sum(self):
+        return self.a + self.b
